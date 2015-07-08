ccb75ad9047ef10f78a6836703e44cab7f26b956
KyrstanZachary <kyrstan.zachary@gmail.com>
2015-07-08 13:02:57 -0700

    problem with href onthe font awesome icons. Same styling.

--- client/assets/js/app.js
+++ client/assets/js/app.js
@@ -51,7 +51,6 @@
     }]
   }]);
 
-
   config.$inject = ['$urlRouterProvider', '$locationProvider'];
 
   function config($urlProvider, $locationProvider) {
@@ -103,9 +102,10 @@
       if(intersect.length > 0) return true
       return false;
     }
+
     $scope.assignments=[
       {
-        title: 'Assignment #1', 
+        title: 'First Day of School', 
         dueDate: '05/31/15', 
         contents: 'the quick brown fox jumps over the lazy dog. this is a sentence that uses every letter of the alphabet.', 
         location: 'homework/homework.pdf',
@@ -118,6 +118,7 @@
         title: 'Assignment #2', 
         dueDate: '06/31/15', 
         contents: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        location: 'homework/homework.pdf',
         subjects: [
             'English'
           ]
@@ -125,16 +126,16 @@
       {title: 'Assignment #3', 
       dueDate: '07/31/15', 
       contents: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum..',
-        subjects: [
+      subjects: [
             'science'
           ]
     },
       {title: 'Assignment #4', 
       dueDate: '08/31/15', 
       contents: 'Prepare to learn the usual.',
-        subjects: [
-            'math',
-            'science'
+      subjects: [
+            'English'
+            
           ]
     }
     ];
